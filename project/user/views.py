d17218fef3ff18f164984d9c2f30e536642f8678
Tosin Damilare James Animashaun <acetakwas@gmail.com>
2015-12-22 14:47:39 +0100

    Fix 404 issue
    Added import to file: <project/user/__init__.py> for module containing view functions: <project/user/views.py>
    
    And a few other fixes.

--- project/user/views.py
+++ project/user/views.py
@@ -1,19 +1,28 @@
 from flask import render_template
-from project import db, user
+
+# line modified by: takwas
+from project import db#, user
+
+# line added by: takwas
+from . import user as user_blueprint
+
 #from project.user import user
 from project.models import Role, User
 from project.user.forms import RegisterForm
 
+print "B:UEPPPP!! {}".format(user_blueprint)
 
 
-
-@user.route('/roles/')
+# line modified by: takwas
+@user_blueprint.route('/roles/')
 def roles():
     # names = [name for name, in db.session.query(Role.name).all()]
-    names = "Wlecome to Gsam"
+    names = "Welcome to Gsam"
+    print "fine"
     return render_template('front/roles.html', names=names)
 
-@user.route('/register')
+# line modified by: takwas
+@user_blueprint.route('/register/')
 def register():
     form = RegisterForm()
     user = User(username=form.username.data, title=form.title.data, first_name=form.first_name.data, last_name=form.last_name.data, other_name=form.other_names.data, password=form.password.data, phone=form.phone.data, email=form.email.data, address=form.address.data)
